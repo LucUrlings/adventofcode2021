5c76da21c95c5d8600b596340fca4998ac392800
LucUrlings <cby.uvh0and1pgq4NHX>
2021-12-09 15:11:45 +0100

    day9 part 1 almost done

--- src/days/9/partOne/main.js
+++ src/days/9/partOne/main.js
@@ -1,4 +1,103 @@
-import { input } from '../input.js'
+import { input } from "../input.js";
 
-const main = (startingPositions) => {}
-console.log(main(input))
+/**
+ *
+ * CooordinatePositions:
+ *    1,  2,  3
+ *    4,  XY, 6
+ *    7,  8,  9
+ *
+ * @param heightMap
+ * @param posX
+ * @param posY
+ */
+const calculateRiskLevelForPosition = (heightMap, posX, posY) => {
+  console.log(posX,posY)
+  const heightXY = heightMap[posY][posX];
+
+  let height1
+  let height2
+  let height3
+
+  let height4
+  let height6
+
+  let height7
+  let height8
+  let height9
+
+  // try {
+  //   height1 = heightMap[posY - 1][posX - 1]
+  // }
+  // catch {}
+
+  try {
+    height2 = heightMap[posY - 1][posX]
+  }
+  catch {}
+
+  // try {
+  //   height3 = heightMap[posY - 1][posX + 1]
+  // }
+  // catch {}
+
+  try {
+    height4 = heightMap[posY][posX - 1]
+  }
+  catch {}
+
+  try {
+    height6 = heightMap[posY][posX + 1]
+  }
+  catch {}
+
+  // try {
+  //   height7 = heightMap[posY + 1][posX - 1]
+  // }
+  // catch {}
+
+  try {
+    height8 = heightMap[posY + 1][posX]
+  }
+  catch {}
+
+  // try {
+  //   height9 = heightMap[posY + 1][posX + 1]
+  // }
+  // catch {}
+
+  // if (!height1) height1 = 10
+  if (!height2) height2 = 10
+  // if (!height3) height3 = 10
+  if (!height4) height4 = 10
+  if (!height6) height6 = 10
+  // if (!height7) height7 = 10
+  if (!height8) height8 = 10
+  // if (!height9) height9 = 10
+
+  console.log(`${height1}  ${height2}  ${height3}
+${height4}  ${heightXY}  ${height6}
+${height7}  ${height8}  ${height9}`)
+
+  // if (heightXY < height1 && heightXY < height2 && heightXY < height3 && heightXY < height4 && heightXY < height6 && heightXY < height7 && heightXY < height8 && heightXY < height9) {
+  //   return heightXY + 1;
+  // }
+
+  if (heightXY < height2 && heightXY < height4 && heightXY < height6 && heightXY < height8) {
+    return heightXY + 1;
+  }
+  console.log("")
+  return 0;
+};
+
+const main = (heightMap) => {
+  let riskLevel = 0;
+
+  heightMap.forEach((heightRow, posY) => {
+    heightRow.forEach((position, posX) => {
+      riskLevel += calculateRiskLevelForPosition(heightMap, posX, posY);
+    });
+  });
+  return riskLevel
+};
+console.log(main(input));
