2d275bdc46094111b2f5034c4822e3ab06ad0baf
Luc Urlings <urlingsluc@gmail.com>
2021-12-09 19:42:44 +0100

    optimized part 1, init part 2

--- src/days/9/partOne/main.js
+++ src/days/9/partOne/main.js
@@ -3,98 +3,49 @@ import { input } from '../input.js'
 /**
  *
  * CooordinatePositions:
- *    1,  2,  3
- *    4,  XY, 6
- *    7,  8,  9
+ *        1
+ *    2,  XY, 3
+ *        4
  *
  * @param heightMap
  * @param posX
  * @param posY
  */
 const calculateRiskLevelForPosition = (heightMap, posX, posY) => {
-  // console.log(posX,posY)
   const heightXY = heightMap[posY][posX]
 
   let height1
+
   let height2
   let height3
 
   let height4
-  let height6
 
-  let height7
-  let height8
-  let height9
+  if (posY - 1 >= 0) height1 = heightMap[posY - 1][posX]
+  else height1 = 99
 
-  // try {
-  //   height1 = heightMap[posY - 1][posX - 1]
-  // }
-  // catch {}
+  if (posX - 1 >= 0) height2 = heightMap[posY][posX - 1]
+  else height2 = 99
 
-  try {
-    if (posY - 1 >= 0)
-      height2 = heightMap[posY - 1][posX]
-    else height2 = 99
-  } catch {
-  }
+  if (posX + 1 < 100) height3 = heightMap[posY][posX + 1]
+  else height3 = 99
 
-  // try {
-  //   height3 = heightMap[posY - 1][posX + 1]
-  // }
-  // catch {}
+  if (posY + 1 < 100) height4 = heightMap[posY + 1][posX]
+  else height4 = 99
 
-  try {
-    if (posX - 1 >= 0)
-      height4 = heightMap[posY][posX - 1]
-    else height4 = 99
-  } catch {
-  }
+  console.log(`
+     ${height1}
+  ${height2}  ${heightXY}  ${height3}
+     ${height4}`)
 
-  try {
-    if (posX + 1 < 100)
-      height6 = heightMap[posY][posX + 1]
-    else height6 = 99
-  } catch {
-  }
-
-  // try {
-  //   height7 = heightMap[posY + 1][posX - 1]
-  // }
-  // catch {}
-
-  try {
-    if (posY + 1 < 100)
-      height8 = heightMap[posY + 1][posX]
-    else height8 = 99
-  } catch {
-  }
-
-  // try {
-  //   height9 = heightMap[posY + 1][posX + 1]
-  // }
-  // catch {}
-
-  // if (!height1) height1 = 10
-      // if (!height2) height2 = 10
-  // if (!height3) height3 = 10
-      // if (!height4) height4 = 10
-      // if (!height6) height6 = 10
-  // if (!height7) height7 = 10
-      // if (!height8) height8 = 10
-  // if (!height9) height9 = 10
-
-  console.log(`${height1}  ${height2}  ${height3}
-${height4}  ${heightXY}  ${height6}
-${height7}  ${height8}  ${height9}`)
-
-  // if (heightXY < height1 && heightXY < height2 && heightXY < height3 && heightXY < height4 && heightXY < height6 && heightXY < height7 && heightXY < height8 && heightXY < height9) {
-  //   return heightXY + 1;
-  // }
-
-  if (heightXY < height2 && heightXY < height4 && heightXY < height6 && heightXY < height8) {
+  if (
+    heightXY < height1 &&
+    heightXY < height2 &&
+    heightXY < height3 &&
+    heightXY < height4
+  ) {
     return heightXY + 1
   }
-  console.log('')
   return 0
 }
 
--- src/days/9/partTwo/main.js
+++ src/days/9/partTwo/main.js
@@ -1,4 +1,62 @@
 import { input } from '../input.js'
 
-const main = (startingPositions) => {}
+/**
+ *
+ * CooordinatePositions:
+ *        1
+ *    2,  XY, 3
+ *        4
+ *
+ * @param heightMap
+ * @param posX
+ * @param posY
+ */
+const findBasins = (heightMap, posX, posY) => {
+  const heightXY = heightMap[posY][posX]
+
+  let height1
+
+  let height2
+  let height3
+
+  let height4
+
+  if (posY - 1 >= 0) height1 = heightMap[posY - 1][posX]
+  else height1 = 99
+
+  if (posX - 1 >= 0) height2 = heightMap[posY][posX - 1]
+  else height2 = 99
+
+  if (posX + 1 < 100) height3 = heightMap[posY][posX + 1]
+  else height3 = 99
+
+  if (posY + 1 < 100) height4 = heightMap[posY + 1][posX]
+  else height4 = 99
+
+  console.log(`
+     ${height1}
+  ${height2}  ${heightXY}  ${height3}
+     ${height4}`)
+
+  if (
+    heightXY < height1 &&
+    heightXY < height2 &&
+    heightXY < height3 &&
+    heightXY < height4
+  ) {
+    return heightXY + 1
+  }
+  return 0
+}
+
+const main = (heightMap) => {
+  let riskLevel = 0
+
+  heightMap.forEach((heightRow, posY) => {
+    heightRow.forEach((position, posX) => {
+      riskLevel += findBasins(heightMap, posX, posY)
+    })
+  })
+  return riskLevel
+}
 console.log(main(input))
