be599e4fe5ea653d92340201b31126c2c441e749
Luc Urlings <urlingsluc@gmail.com>
2021-12-10 16:43:23 +0100

    day 10 part 2 done

--- src/days/10/partTwo/main.js
+++ src/days/10/partTwo/main.js
@@ -1,4 +1,67 @@
 import { input } from '../input.js'
 
-const main = (startingPositions) => {}
+const removeCompleteSequences = (lineCopy) => {
+  let removing = true
+  while (removing) {
+    const lineCopyOld = lineCopy
+    lineCopy = lineCopy
+      .replace('<>', '')
+      .replace('()', '')
+      .replace('{}', '')
+      .replace('[]', '')
+    if (lineCopy.length === lineCopyOld.length) removing = false
+
+    console.log(lineCopy)
+  }
+
+  return lineCopy
+}
+
+const processLine = (line) => {
+  let lineCopy = line
+
+  lineCopy = removeCompleteSequences(lineCopy)
+
+  if (lineCopy.includes(')')) return 0
+  else if (lineCopy.includes(']')) return 0
+  else if (lineCopy.includes('}')) return 0
+  else if (lineCopy.includes('>')) return 0
+
+  let score = 0
+
+  while (lineCopy.length !== 0) {
+    const last = lineCopy.charAt(lineCopy.length - 1)
+    switch (last) {
+      case '(':
+        lineCopy = lineCopy + ')'
+        score = score * 5 + 1
+        break
+      case '[':
+        lineCopy = lineCopy + ']'
+        score = score * 5 + 2
+        break
+      case '{':
+        lineCopy = lineCopy + '}'
+        score = score * 5 + 3
+        break
+      case '<':
+        lineCopy = lineCopy + '>'
+        score = score * 5 + 4
+        break
+    }
+    lineCopy = removeCompleteSequences(lineCopy)
+  }
+
+  return score
+}
+
+const main = (lines) => {
+  let total = []
+  lines.forEach((line) => {
+    total.push(processLine(line))
+  })
+
+  total = total.filter((score) => score !== 0).sort((a, b) => a - b)
+  return total[Math.floor(total.length / 2)]
+}
 console.log(main(input))
