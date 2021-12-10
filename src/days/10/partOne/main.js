19c07a0bf65d9b2e9b6d389c21479b9813573b8c
Luc Urlings <urlingsluc@gmail.com>
2021-12-10 16:20:38 +0100

    day 10 part 1 done

--- src/days/10/partOne/main.js
+++ src/days/10/partOne/main.js
@@ -1,4 +1,49 @@
 import { input } from '../input.js'
 
-const main = (startingPositions) => {}
+const processLine = (line) => {
+  let lineCopy = line
+
+  let removing = true
+  while (removing) {
+    const lineCopyOld = lineCopy
+    lineCopy = lineCopy
+      .replace('<>', '')
+      .replace('()', '')
+      .replace('{}', '')
+      .replace('[]', '')
+    if (lineCopy.length === lineCopyOld.length) removing = false
+
+    console.log(lineCopy)
+  }
+
+  for (let i = 0; i < lineCopy.length; i++) {
+    switch (lineCopy.charAt(i)) {
+      case ')':
+        return 3
+      case ']':
+        return 57
+      case '}':
+        return 1197
+      case '>':
+        return 25137
+      case '(':
+      case '[':
+      case '{':
+      case '<':
+      default:
+        break
+    }
+  }
+
+  return 0
+}
+
+const main = (lines) => {
+  let total = 0
+  lines.forEach((line) => {
+    total += processLine(line)
+  })
+
+  return total
+}
 console.log(main(input))
